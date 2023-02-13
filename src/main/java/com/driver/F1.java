2c77e9ecad9dde7342f674a7948476b46600789e
Angad <anilterapalli@gmail.com>
2023-02-13 13:26:51 +0530

    Vehicles assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-       super(name, 4, 4, 6, isManual, "RangeRollar", 6);
+       super(name, 4, 4, 6, isManual, "BMW", 6);
     }
 
     public void accelerate(int rate){
@@ -25,8 +25,9 @@ public class F1 extends Car {
             stop();
             this.changeGear(1);
         }
+        //for all other cases, change the gear accordingly
         else if(newSpeed > 0 && newSpeed <= 50){
-           this.changeGear(1);
+            this.changeGear(1);
         }else if(newSpeed <= 100){
             changeGear(2);
         }else if(newSpeed <= 150){
@@ -38,7 +39,7 @@ public class F1 extends Car {
         }else{
             changeGear(6);
         }
-        //for all other cases, change the gear accordingly
-        changeSpeed(newSpeed, getCurrentDirection());
+
+        if(newSpeed > 0)  changeSpeed(newSpeed, getCurrentDirection());
     }
 }
